8f28e9d753f338fcda34fa44dd28f3484ae0a2f1
marcinforma <122903915+marcinforma@users.noreply.github.com>
2026-05-20 20:17:12 +0200

    Update and rename lotto.py to palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+def czy_palindrom(wyraz):
+    return wyraz.lower() == wyraz[::-1].lower()
+
+
+tekst = input('Podaj słowo: ')
+
+
+if czy_palindrom(tekst):
+    print(f'Słowo "{tekst}" jest palindromem.')
+else:
+    print(f'Słowo "{tekst}" NIE jest palindromem.')
